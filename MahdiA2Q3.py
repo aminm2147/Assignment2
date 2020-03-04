685d92b9cf3cbd7182a3b52bb7068af7dcdf85d6
pouria <61024139+aminm2147@users.noreply.github.com>
2020-03-04 18:39:39 -0500

    changed to a new one

--- MahdiA2Q3.py
+++ MahdiA2Q3.py
@@ -1,4 +1,3 @@
-
 # ICS3UI-02 for Ms. Harris
 
 # Mahdi Amini
@@ -13,123 +12,104 @@
 # date started: 02/28/2020
 # date finished: 02/29/2020
 
-# this program draw the Canada flag and red stars in random spots
+# this program draw a shape and fill its surrounding to a color from rainbow colors
+# this program also delete every thing it just drew
+
+
 import turtle
 from turtle import *  # using turtle library
-from random import randint  # using random library and randint module
+from random import randrange  # using random library and randint module
 
 
-def rand_star():
-    """drawing stars in random places except the flag position"""
-    turtle.up()
-    turtle.color("red", "yellow")  # the color of stars is red
-
-    while True:
-        x = randint(-200, 200)
-        y = randint(-200, 200)
-        turtle.goto(x, y)  # go to random spots
-        while True:  # not putting stars on the flag
-            if 240 > x > -120 and 60 > y > 60:
-                x = randint(-200, 200)
-                y = randint(-200, 200)
-            else:
-                break
-        for i in range(15):  # drawing 15 stars in random spots
-            turtle.speed(9)
-            turtle.down()  # start drawing
-            turtle.begin_fill()
-            turtle.forward(20)
-            turtle.left(170)
-            turtle.end_fill()
-        turtle.up()  # do not draw any more
+def undo_maker():  # defining a function to delete every things which have drawn
+    for back in range(10000):
+        turtle.undo()
 
 
-# change the color to red
-turtle.color('red', 'red')
-# change the shape of the turtle to turtle
+def color_rainbow():  # defining a function to set the color to a random
+    rancor = randrange(1, 8)
+    if rancor == 1:
+        turtle.color("#9400D3")  # color VIOLET
+    if rancor == 2:
+        turtle.color("#4B0082")  # color INDIGO
+    if rancor == 3:
+        turtle.color("#0000FF")  # color BLUE
+    if rancor == 4:
+        turtle.color("#00FF00")  # color GREEN
+    if rancor == 5:
+        turtle.color("#FFFF00")  # color YELLOW
+    if rancor == 6:
+        turtle.color("#FF7F00")   # color ORANGE
+    if rancor == 7:
+        turtle.color("#FF0000")  # color RED
+
+#  set the color to light blue
+turtle.color("#33cddb")
+turtle.pencolor("black")  # set the color of the pen to black
 turtle.shape("turtle")
-# the beginning of the coloring part
-turtle.begin_fill()
-turtle.up()  # not to draw any thing
-turtle.goto(-60, -60)  # go to the position
-turtle.down()  # start drawing
-turtle.left(90)  # drawing the left side og the flag
-turtle.forward(120)
-turtle.left(90)
-turtle.forward(60)
-turtle.left(90)
-turtle.forward(120)
-turtle.left(90)
-turtle.forward(60)
-turtle.end_fill()  # coloring the rectangle
-turtle.up()  # not draw any thing
-turtle.goto(180, -60)  # move to other position
-turtle.down()  # drawing the right side of the flag
-turtle.begin_fill()
-turtle.left(90)
-turtle.forward(120)
-turtle.left(90)
-turtle.forward(60)
-turtle.left(90)
-turtle.forward(120)
-turtle.left(90)
-turtle.forward(60)
-turtle.end_fill()  # coloring the rectangle
-turtle.up()  # not draw any thing
-turtle.goto(40, -60)  # move to logo position
-turtle.down()  # start drawing the logo
-turtle.begin_fill()
-turtle.forward(6)
-turtle.left(100)
-turtle.forward(30)
-turtle.right(120)
-turtle.forward(20)
-turtle.left(105)
-turtle.forward(10)
-turtle.right(45)
-turtle.forward(50)
-turtle.left(120)
-turtle.forward(10)
-turtle.right(100)
-turtle.forward(20)
-turtle.left(120)
-turtle.forward(20)
-turtle.right(80)
-turtle.forward(10)
-turtle.left(120)
-turtle.forward(27)
-turtle.right(135)
-turtle.forward(40)
-turtle.left(135)
-turtle.forward(15)
-turtle.right(100)
-turtle.forward(30)
-turtle.left(120)
-turtle.forward(30)
-turtle.right(110)
-turtle.forward(15)
-turtle.left(145)
-turtle.forward(40)
-turtle.right(130)
-turtle.forward(27)
-turtle.left(130)
-turtle.forward(10)
-turtle.right(90)
-turtle.forward(20)
-turtle.left(110)
-turtle.forward(20)
-turtle.right(100)
-turtle.forward(10)
-turtle.left(125)
-turtle.forward(50)
-turtle.right(50)
-turtle.forward(10)
-turtle.left(120)
-turtle.forward(20)
-turtle.right(125)
-turtle.forward(30)
-turtle.goto(40, -60)
-turtle.end_fill()  # color the logo
-turtle.hideturtle()  # the the drawing turtle
-rand_star()  # calling the function rand_star
+for i in range(5):  # draw the inner 5 kits
+    turtle.begin_fill()
+    turtle.right(36)
+    turtle.forward(50)
+    turtle.right(72)
+    turtle.forward(50)
+    turtle.right(108)
+    turtle.forward(50)
+    turtle.right(72)
+    turtle.forward(50)
+    turtle.end_fill()
+
+
+turtle.color("#269518")  # set the color to dark green
+turtle.pencolor("black")  # set the color of the pen to black
+for m in range(5):  # draw the outer 5 green kits
+    turtle.begin_fill()
+    turtle.right(36)
+    turtle.forward(50)
+    turtle.right(72)
+    turtle.forward(50)
+    turtle.left(144)
+    turtle.forward(50)
+    turtle.left(36)
+    turtle.forward(50)
+    turtle.left(144)
+    turtle.forward(50)
+    turtle.right(72)
+    turtle.forward(50)
+    turtle.end_fill()
+
+for i in range(5):  # draw the surrounding kits in the color of rainbow
+    turtle.right(36)
+    turtle.right(72)
+    turtle.forward(50)
+    turtle.left(72)
+    turtle.forward(50)
+    turtle.right(144)
+    turtle.forward(50)
+    for j in range(2):
+        color_rainbow()  # use the rainbow function which sets the color to a random color in rainbow
+        turtle.pencolor("black")
+        turtle.begin_fill()  # start the filling
+        turtle.left(36)
+        turtle.forward(50)
+        turtle.right(72)
+        turtle.forward(50)
+        turtle.right(108)
+        turtle.forward(50)
+        turtle.right(72)
+        turtle.forward(50)
+        turtle.end_fill()  # finish the filling
+    turtle.left(36)
+    turtle.forward(50)
+    turtle.right(144)
+    turtle.forward(50)
+    turtle.left(72)
+    turtle.forward(50)
+
+undo_maker()
+turtle.write("bye", move=True, font=("Arial", 20, "normal"))  # write bye in the center of the screen
+
+
+turtle.hideturtle()  # hide the shape of the drawing turtle
+
 done()  # finish the process
